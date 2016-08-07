69128fc958fa6128f8e4bb1ee217d3fc0f7865e6
Maxim Topciu <maximtop@gmail.com>
2016-08-07 20:01:22 +0300

    Refactoring

--- src/Utils.php
+++ src/Utils.php
@@ -24,7 +24,7 @@ function printReport($path)
 
     foreach ($files as $filename) {
         $fileContent = file_get_contents($filename);
-        $errorList = getFunctions($fileContent);
+        $errorList = getErrors($fileContent);
         $climate = new \League\CLImate\CLImate;
         $climate->out($filename);
 
--- src/Visitor.php
+++ src/Visitor.php
@@ -9,24 +9,24 @@ use PhpParser\Node\Stmt\Function_;
 
 class Visitor extends NodeVisitorAbstract
 {
-    private $errorFunctions = [];
+    private $errors = [];
 
     public function leaveNode(Node $node)
     {
         if ($node instanceof ClassMethod && checkFunctionName($node->name) != true) {
             $errorString = "Method name is not in camel caps format";
-            $this->errorFunctions[] = [$node->getLine(), "<red>error</red>", $errorString, $node->name];
+            $this->errors[] = [$node->getLine(), "<red>error</red>", $errorString, $node->name];
         } elseif ($node instanceof Function_ && checkFunctionName($node->name) != true) {
             $errorString = "Function name is not in camel caps format";
-            $this->errorFunctions[] = [$node->getLine(), "<red>error</red>", $errorString, $node->name];
+            $this->errors[] = [$node->getLine(), "<red>error</red>", $errorString, $node->name];
         }
     }
 
-    public function getErrorFunctions()
+    public function getErrors()
     {
-        if (count($this->errorFunctions) <= 0) {
+        if (count($this->errors) <= 0) {
             return "There is no errors in functions or methods names";
         }
-        return $this->errorFunctions;
+        return $this->errors;
     }
 }
